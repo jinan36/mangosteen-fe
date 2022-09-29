d032ab8f4241a0135fe180f6e14d81ff0cd53049
jinan36 <a492073467@gmail.com>
2022-09-30 00:12:09 +0800

    feat: 四个欢迎页面

--- src/components/welcome/Forth.tsx
+++ src/components/welcome/Forth.tsx
@@ -1,7 +1,53 @@
 import { defineComponent } from "vue";
+import { RouterLink } from "vue-router";
+import cloud from "../../assets/icons/cloud.svg";
 
 export const Forth = defineComponent({
   setup() {
-    return () => <div>Forth</div>;
+    return () => (
+      <div flex="~ col grow" pb="$welcome-nav-gap">
+        <div
+          flex="~ grow col"
+          bg="$welcome-card-bg"
+          rd-8px
+          p16px
+          m="x16px t16px"
+          justify-center
+          items-center
+          text-center
+        >
+          <img src={cloud} />
+          <h2 mt40px>
+            每日提醒
+            <br />
+            不遗漏每一笔账单
+          </h2>
+        </div>
+        <div
+          fixed
+          z="$z-index-bottom-nav"
+          bg="$welcome-bg-end"
+          left-0
+          bottom-0
+          w-full
+          flex
+          justify-between
+          items-center
+          text-24px
+          h="$welcome-nav-height"
+          px16px
+          c="$welcome-text"
+          pb="$welcome-nav-gap"
+        >
+          <RouterLink invisible to="/start">
+            跳过
+          </RouterLink>
+          <RouterLink to="/start">完成</RouterLink>
+          <RouterLink invisible to="/start">
+            跳过
+          </RouterLink>
+        </div>
+      </div>
+    );
   },
 });
