da74413a2d24fc286ce8675b6ae842c1634825b5
jinan36 <a492073467@gmail.com>
2022-10-01 14:44:55 +0800

    feat: Icon 组件

---
+++ src/shared/Icon.tsx
@@ -0,0 +1,22 @@
+import { defineComponent, PropType } from "vue";
+
+export type IconNames =
+  | "add"
+  | "chart"
+  | "clock"
+  | "cloud"
+  | "mangosteen"
+  | "pig";
+
+export const Icon = defineComponent({
+  props: {
+    name: { type: String as PropType<IconNames>, required: true },
+  },
+  setup(props) {
+    return () => (
+      <svg w="1.2em" h="1.2em">
+        <use xlinkHref={"#" + props.name} />
+      </svg>
+    );
+  },
+});
