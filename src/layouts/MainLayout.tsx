9eafb46476a5dbad761ce6e8eece49c3c3c1cfd3
jinan36 <a492073467@gmail.com>
2022-10-02 22:47:27 +0800

    feat: MainLayout 组件

---
+++ src/layouts/MainLayout.tsx
@@ -0,0 +1,18 @@
+import { defineComponent } from "vue";
+import { Navbar } from "../shared/Navbar";
+
+export const MainLayout = defineComponent({
+  setup(_, { slots }) {
+    return () => (
+      <div>
+        <Navbar>
+          {{
+            default: () => slots.title?.(),
+            icon: () => slots.icon?.(),
+          }}
+        </Navbar>
+        {slots.default?.()}
+      </div>
+    );
+  },
+});
--- src/views/StartPage.tsx
+++ src/views/StartPage.tsx
@@ -1,10 +1,10 @@
 import { defineComponent } from "vue";
 import { RouterLink } from "vue-router";
+import { MainLayout } from "../layouts/MainLayout";
 import { Button } from "../shared/Button";
 import { Center } from "../shared/Center";
 import { FloatButton } from "../shared/FloatButton";
 import { Icon } from "../shared/Icon";
-import { Navbar } from "../shared/Navbar";
 import { Overlay } from "../shared/overlay/Overlay";
 
 export const StartPage = defineComponent({
@@ -14,37 +14,39 @@ export const StartPage = defineComponent({
       overlayVisible = !overlayVisible;
     };
     return () => (
-      <div>
-        <Navbar>
-          {{
-            default: () => "山竹记账",
-            icon: () => (
-              <Icon
-                w="30px"
-                h="30px"
-                relative="~"
-                top="2px"
-                name="menu"
-                onClick={onClickMenu}
-              ></Icon>
-            ),
-          }}
-        </Navbar>
-        <Center p="y120px">
-          <Icon name="pig" w="128px" h="128px" />
-        </Center>
-        <div p="16px">
-          <RouterLink to="/items/create">
-            <Button w="full">开始记账</Button>
-          </RouterLink>
-        </div>
-        <RouterLink to="/items/create">
-          <FloatButton iconName="add" />
-        </RouterLink>
-        {overlayVisible && (
-          <Overlay onClose={() => (overlayVisible = false)}></Overlay>
-        )}
-      </div>
+      <MainLayout>
+        {{
+          title: () => "山竹记账",
+          icon: () => (
+            <Icon
+              w="30px"
+              h="30px"
+              relative="~"
+              top="2px"
+              name="menu"
+              onClick={onClickMenu}
+            ></Icon>
+          ),
+          default: () => (
+            <>
+              <Center p="y120px">
+                <Icon name="pig" w="128px" h="128px" />
+              </Center>
+              <div p="16px">
+                <RouterLink to="/items/create">
+                  <Button w="full">开始记账</Button>
+                </RouterLink>
+              </div>
+              <RouterLink to="/items/create">
+                <FloatButton iconName="add" />
+              </RouterLink>
+              {overlayVisible && (
+                <Overlay onClose={() => (overlayVisible = false)}></Overlay>
+              )}
+            </>
+          ),
+        }}
+      </MainLayout>
     );
   },
 });
