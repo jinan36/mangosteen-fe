0002d6abe648349ce886c1f4243a5265aea28010
jinan36 <a492073467@gmail.com>
2022-10-09 10:38:23 +0800

    feat: 按钮支持等级

--- src/shared/Button.tsx
+++ src/shared/Button.tsx
@@ -1,12 +1,28 @@
+import type { PropType } from 'vue'
 import { defineComponent } from 'vue'
 
 export const Button = defineComponent({
-  setup(_, { slots }) {
+  props: {
+    onClick: {
+      type: Function as PropType<(e: MouseEvent) => void>,
+    },
+    level: {
+      type: String as PropType<'important' | 'normal' | 'danger'>,
+      default: 'important',
+    },
+  },
+  setup(props, { slots }) {
+    const levelClassMap = {
+      important: '',
+      normal: '',
+      danger: 'border-$button-bg-danger bg-$button-bg-danger',
+    }
+
     return () => (
       <button
-        border="~ $button-bg rd-$button-radius"
+        class={['border-$button-bg bg-$button-bg', levelClassMap[props.level]]}
+        border="~ rd-$button-radius"
         p="y8px x16px"
-        bg="$button-bg"
         text="$button-text size-$button-font-size"
         h="$button-height"
       >
