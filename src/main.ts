d8b51c72f3bd2d83fd523e4405f2dff77906d648
jinan36 <a492073467@gmail.com>
2022-10-09 14:04:26 +0800

    fix: 移动 unocss 入口文件至最前，方便覆盖

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,11 @@
+import 'uno.css'
+import 'svg.store'
+
 import { createApp } from 'vue'
 import { createRouter } from 'vue-router'
 import { App } from './App'
 import { routes } from './config/routes'
 import { history } from './shared/history'
-
-import 'uno.css'
-import 'svg.store'
 
 const router = createRouter({ history, routes })
 
