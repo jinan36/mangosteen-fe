41d8b6aa3647763d1e0ccb82956941d3218e32ad
jinan36 <a492073467@gmail.com>
2022-10-11 11:23:50 +0800

    chore: 移动

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { createRouter } from 'vue-router'
 import { routes } from './config/routes'
 import { history } from './shared/history'
 import { App } from './App'
-import './main.styl'
+import './styles/main.styl'
 
 const router = createRouter({ history, routes })
 
